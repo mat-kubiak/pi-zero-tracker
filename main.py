369ec2656665d3cede48acbd6db7c55dd50f9e46
Mateusz Kubiak <matkubiak888@gmail.com>
2023-11-30 17:15:17 +0100

    feat: test

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import os
 import socket
 
+from bluepy.btle import Scanner, DefaultDelegate
+from datetime import datetime
+import time
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev or isNewData:
+            beacon_info = f"Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, Name: {dev.getValueText(9)}, UUID: {dev.addr}, RSSI: {dev.rssi}"
+            if dev.getValueText(9) == 'J3bane_G0wno':
+                print(beacon_info)
+                with open("beacon_data.txt", "a") as file:
+                    file.write(beacon_info + "\n")
+
+scan_duration = 60  # 10 minutes
+scanner = Scanner().withDelegate(ScanDelegate())
+start_time = time.time()
+while time.time() - start_time < scan_duration:
+    devices = scanner.scan(1)
+    time.sleep(1) #break between scans
+
 class info:
     script_dir = os.path.dirname(os.path.realpath(__file__))
     logfile_path = os.path.join(script_dir, 'log')

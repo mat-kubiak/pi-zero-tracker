c234e11088d82b7196aa31233ef2de90795c1969
Filip Andrzejewski <Andre.gameplay@o2.pl>
2024-01-11 16:25:25 +0100

    Movement duplication prevention (not tested)

--- src/rssi_pairmaker.py
+++ src/rssi_pairmaker.py
@@ -1,9 +1,19 @@
-# dummy_sensor_data = [[1704282974, 1704282975, 1704282976, 1704282977, 1704282978, 1704282979, 1704282980],
+# dummy_sensor_data = [[17042829740, 17042829750, 17042829760, 17042829770, 17042829780, 17042829790, 17042829800],
 #                      [-1000, -59, -1000, -61, -1000, -59, -1000],
 #                      [-58, -1000, -1000, -67, -1000, -56, -1000],
-#                      [-1000, -56, -1000, -65, -1000, -1000, -54]]
+#                      [-1000, -56, -1000, -65, -1000, -1000, -54]
+#                      ]
+#
+#   dummy data structure:   [[time values, ...],
+#                             [first sensor data, ...],
+#                             [second sensor data, ...], ...
+#                           ]
+#
+#                           sensor_data[raspberry_no][measurement_no]
+#                           raspberry_no == 0 -> time_value
+#
 
-def check_rssi(args):
+def return_strongest_index(args):
     if all(arg == -1000 for arg in args):
         return 0
     else:
@@ -15,29 +25,52 @@ def check_rssi(args):
                 max_arg_index = i
         return 0 if max_arg_index is None else max_arg_index + 1
 
+
+maximal_motion_speed = 3.5  # meters / second
+minimal_distance_between_sensors = 10  # meters
+minimal_travel_time = int((minimal_distance_between_sensors / maximal_motion_speed) * 10)  # in tens of a second
+
+
 def create_connections(sensor_data):
 
-    times_no_signal = 0
     pairs = []
-    values = []
+    rssi_values = []
+    last_recorded_signal_timestamp = 0
 
-    # Do the procedure for the first time. Do not worry about lack of signal. First and last scan always has signal
+    # Do the procedure for the first time
+    # First and last scan always has signal from the closest sensor
     for j in range(1, len(sensor_data)):
-        values.append(sensor_data[j][0])
-    active_raspberry = check_rssi(values)
+        rssi_values.append(sensor_data[j][0])
+
+    active_raspberry = return_strongest_index(rssi_values)
+    # active raspberry = closest to the beacon at the moment
     pairs.append((active_raspberry,))
 
-    last_pair = 0
+    last_pair_index = 0
     for i in range(1, len(sensor_data[0])):
-        values = []
+        rssi_values = []
         for j in range(1, len(sensor_data)):
-            values.append(sensor_data[j][i])
+            rssi_values.append(sensor_data[j][i])
 
-        active_raspberry = check_rssi(values)
-        if pairs[last_pair][0] != active_raspberry and active_raspberry: #signals does not stay still. Beacon moves to different raspberry
-            pairs[last_pair] += (active_raspberry,)
-            last_pair += 1
-            pairs.append((active_raspberry,))
+        active_raspberry = return_strongest_index(rssi_values)
+        if sensor_data[0][i] - last_recorded_signal_timestamp > minimal_travel_time:
+            # if move happened too fast it is regarded as an error (caused by signal spike)
+            if pairs[last_pair_index][0] != active_raspberry and active_raspberry:
+                # last signal is not the same as new one -> beacon moves closer to different raspberry
+                pairs[last_pair_index] += (active_raspberry,)
+                last_pair_index += 1
+                pairs.append((active_raspberry,))
+                last_recorded_signal_timestamp = sensor_data[0][i]
+                
 
     pairs.pop()
     return pairs
+
+
+dummy_sensor_data = [[17042829740, 17042829750, 17042829760, 17042829770, 17042829780, 17042829790, 17042829800],
+                     [-1000, -59, -1000, -61, -1000, -59, -1000],
+                     [-58, -1000, -1000, -67, -1000, -56, -1000],
+                     [-1000, -56, -1000, -65, -1000, -1000, -54]
+                     ]
+
+print(create_connections(dummy_sensor_data))
